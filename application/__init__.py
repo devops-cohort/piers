113228e1b8c2ccbf0b2e06ebbe5eb2e8988c97d5
PCMBarber <Kochab.96@gmail.com>
2019-12-05 16:42:23 +0000

    mostly done

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from os import getenv
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:CHANGETHIS@35.246.48.37/card_game'
-app.config['SECRET_KEY'] = 'CHANGE THIS'
+app.config['SQLALCHEMY_DATABASE_URI'] = ("mysql+pymysql://" + getenv('MYSQL_USER') + ":" + getenv('MYSQL_PWD') + "@" + getenv('MYSQL_IP') + "/" + getenv('MYSQL_DB'))
+app.config['SECRET_KEY'] = getenv('MYSQL_SK')
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
