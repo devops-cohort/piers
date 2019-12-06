6d89c0c2f54c6ed5130424fa6bf2cbd1f678ff3e
Piers Barber <Kochab.96@Gmail.com>
2019-12-06 17:56:15 +0000

    Debugging

--- application/routes.py
+++ application/routes.py
@@ -190,7 +190,7 @@ def deck(deck_id):
                 name.append(card.card_name)
                 attk.append(card.card_attk)
                 defense.append(card.card_def)
-        return render_template('deck.html', title=deck_id, card_name=name, card_attk=attk, card_def=defence, count=len(name), , deck_id=deck_id)
+        return render_template('deck.html', title=deck_id, card_name=name, card_attk=attk, card_def=defence, count=len(name), deck_id=deck_id)
     else:
         return render_template('deck.html', title=deck_id, count=0, deck_id=deck_id)
 
